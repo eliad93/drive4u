1cf349e56fb875fe2e6d5f738f6d751c3e40f983
yanivbaldi <yaniv-b@campus.technion.ac.il>
2018-12-17 14:59:33 +0200

    working on schedule lesson

--- app/src/main/java/com/example/eliad/drive4u/activities/StudentScheduleLessonActivity.java
+++ app/src/main/java/com/example/eliad/drive4u/activities/StudentScheduleLessonActivity.java
@@ -1,13 +1,56 @@
 package com.example.eliad.drive4u.activities;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.example.eliad.drive4u.R;
+
+import java.util.Calendar;
 
 public class StudentScheduleLessonActivity extends AppCompatActivity {
+
+    private TextView SelectedDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_schedule_lesson);
+
+        SelectedDate = (TextView) findViewById(R.id.selected_date);
+
+        SelectedDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        StudentScheduleLessonActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = month + "/" + day + "/" + year;
+                SelectedDate.setText(date);
+            }
+        };
+
     }
 }
