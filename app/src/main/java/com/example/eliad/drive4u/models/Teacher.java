259af42e4f1967c58a56ab384c5e698784b198a7
yanivbaldi <yaniv-b@campus.technion.ac.il>
2018-12-17 15:28:14 +0200

    merge fix

--- app/src/main/java/com/example/eliad/drive4u/models/Teacher.java
+++ app/src/main/java/com/example/eliad/drive4u/models/Teacher.java
@@ -5,11 +5,10 @@ import java.util.Map;
 
 public class Teacher extends User {
     private HashMap<String, Student> students=null; // key is uId from firebase auth
-<<<<<<< HEAD
-    private Integer price;
-=======
+
+
     private Integer price=100;
->>>>>>> b1aeba7eb1103f97abf7a8479165562e0e49a262
+
 
     public Teacher(String mId, String mName, String mPhoneNumber, String mEmail) {
         super(mId, mName, mPhoneNumber, mEmail);
@@ -36,8 +35,6 @@ public class Teacher extends User {
 
     public void setStudents(HashMap<String, Student> students) {
         this.students = students;
-<<<<<<< HEAD
-=======
     }
 
     public Integer getPrice() {
@@ -46,6 +43,5 @@ public class Teacher extends User {
 
     public void setPrice(Integer price) {
         this.price = price;
->>>>>>> b1aeba7eb1103f97abf7a8479165562e0e49a262
     }
 }
